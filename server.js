479e71120979717939f3df39f2339fe31cbc6263
stepanenko <serhiy.stepanenko@aiesec.net>
2019-12-13 17:26:02 +0200

    added pages to serve

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.end('<h1>Homepage</h1>');
+  if (req.url === '/') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'home.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'ContentType': 'text/html' });
+        res.end(content);
+      }
+    )
+  }
+
 });
 
 const PORT = process.env.PORT || 5000;
